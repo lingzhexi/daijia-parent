f48055ba9ed4874058de47215750b42fcaa95d1b
geekcode <lingzhexi@163.com>
2024-09-27 20:04:51 +0800

    后端模板代码

---
+++ web/web-customer/src/main/java/com/atguigu/daijia/customer/controller/CouponController.java
@@ -0,0 +1,18 @@
+package com.atguigu.daijia.customer.controller;
+
+import com.atguigu.daijia.customer.service.CouponService;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@Tag(name = "优惠券活动接口管理")
+@RestController
+@RequestMapping(value="/coupon")
+@SuppressWarnings({"unchecked", "rawtypes"})
+public class CouponController {
+	
+
+}
+
---
+++ web/web-customer/src/main/java/com/atguigu/daijia/customer/service/impl/CustomerServiceImpl.java
@@ -0,0 +1,16 @@
+package com.atguigu.daijia.customer.service.impl;
+
+import com.atguigu.daijia.customer.client.CustomerInfoFeignClient;
+import com.atguigu.daijia.customer.service.CustomerService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@SuppressWarnings({"unchecked", "rawtypes"})
+public class CustomerServiceImpl implements CustomerService {
+
+
+
+}
