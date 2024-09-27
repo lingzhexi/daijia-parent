10d582878b188aa209086240317431b419ebb68f
geekcode <lingzhexi@163.com>
2024-09-28 01:26:21 +0800

    添加微信小程序依赖以及相关配置

---
+++ service/service-customer/src/main/java/com/atguigu/daijia/customer/config/WxConfigOperator.java
@@ -0,0 +1,28 @@
+package com.atguigu.daijia.customer.config;
+
+import cn.binarywang.wx.miniapp.api.WxMaService;
+import cn.binarywang.wx.miniapp.api.impl.WxMaServiceImpl;
+import cn.binarywang.wx.miniapp.config.WxMaConfig;
+import cn.binarywang.wx.miniapp.config.impl.WxMaDefaultConfigImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+@Component
+public class WxConfigOperator {
+
+    @Autowired
+    private WxConfigProperties wxConfigProperties;
+    @Bean
+    private WxMaService wxMaService() {
+
+        //微信小程序id和密钥
+        WxMaDefaultConfigImpl wxMaConfig = new WxMaDefaultConfigImpl();
+        wxMaConfig.setAppid(wxConfigProperties.getAppId());
+        wxMaConfig.setSecret(wxConfigProperties.getSecret());
+
+        WxMaService wxMaService = new WxMaServiceImpl();
+        wxMaService.setWxMaConfig(wxMaConfig);
+        return wxMaService;
+    }
+}
