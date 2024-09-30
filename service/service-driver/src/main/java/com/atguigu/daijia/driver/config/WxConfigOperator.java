c1f81cb395c0203ae60ccf666e929739753516cb
geekcode <lingzhexi@163.com>
2024-10-01 00:02:03 +0800

    司机登录：司机微信相关配置-修正

--- service/service-driver/src/main/java/com/atguigu/daijia/driver/config/WxConfigOperator.java
+++ service/service-driver/src/main/java/com/atguigu/daijia/driver/config/WxConfigOperator.java
@@ -4,19 +4,21 @@ import cn.binarywang.wx.miniapp.api.WxMaService;
 import cn.binarywang.wx.miniapp.api.impl.WxMaServiceImpl;
 import cn.binarywang.wx.miniapp.config.impl.WxMaDefaultConfigImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 @Component
 public class WxConfigOperator {
     @Autowired
-    WxConfigProperties wxConfigProperties;
+    private WxConfigProperties wxConfigProperties;
 
+    @Bean
     public WxMaService wxMaService() {
 
         //微信默认配置小程序密钥和appid
         WxMaDefaultConfigImpl wxMaDefaultConfig = new WxMaDefaultConfigImpl();
         wxMaDefaultConfig.setSecret(wxConfigProperties.getSecret());
-        wxMaDefaultConfig.setAppid(wxMaDefaultConfig.getAppid());
+        wxMaDefaultConfig.setAppid(wxConfigProperties.getAppId());
 
         //配置绑定到微信服务
         WxMaServiceImpl wxMaService = new WxMaServiceImpl();
