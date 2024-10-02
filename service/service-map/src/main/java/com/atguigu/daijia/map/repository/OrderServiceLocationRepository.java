b548171086e1e3aa518fd4874c95c268e7289c3a
geekcode <lingzhexi@163.com>
2024-10-02 12:15:00 +0800

    模板配置：修改模板依赖问题

--- service/service-map/src/main/java/com/atguigu/daijia/map/repository/OrderServiceLocationRepository.java
+++ service/service-map/src/main/java/com/atguigu/daijia/map/repository/OrderServiceLocationRepository.java
@@ -1,10 +1,8 @@
 package com.atguigu.daijia.map.repository;
 
-import com.atguigu.daijia.model.entity.map.OrderServiceLocation;
-import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface OrderServiceLocationRepository extends MongoRepository<OrderServiceLocation, String> {
+public interface OrderServiceLocationRepository {
 
 }
