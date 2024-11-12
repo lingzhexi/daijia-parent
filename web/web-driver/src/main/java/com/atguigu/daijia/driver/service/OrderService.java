787503a18c2e44e44b02e4cc7881b9612c27aa4c
geekfox <lingzhexi@163.com>
2024-11-12 09:24:20 +0800

    司机：司机抢单及抢单优化问题（乐观锁）补充

--- web/web-driver/src/main/java/com/atguigu/daijia/driver/service/OrderService.java
+++ web/web-driver/src/main/java/com/atguigu/daijia/driver/service/OrderService.java
@@ -1,4 +1,16 @@
 package com.atguigu.daijia.driver.service;
 
+import com.atguigu.daijia.common.result.Result;
+import com.atguigu.daijia.model.vo.order.NewOrderDataVo;
+
+import java.util.List;
+
 public interface OrderService {
+
+    List<NewOrderDataVo> findNewOrderQueueData(Long driverId);
+
+    Boolean clearNewOrderQueueData(Long driverId);
+
+    Boolean robNewOrder(Long driverId, Long orderId);
+
 }
